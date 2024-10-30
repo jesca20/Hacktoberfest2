16af6cfcb16ab3417d6a1eafc3b1b23388164db9
Jessica <72647944+jesca20@users.noreply.github.com>
2024-10-30 20:58:08 +0545

    Update prime.py

--- Python/prime.py
+++ Python/prime.py
@@ -11,3 +11,30 @@ for num in range(lower, upper + 1):
                break
        else:
            print(num) 
+
+
+# using recursion : 
+
+from math import sqrt
+# prime function to check given number prime or not
+def Prime(number, itr):  
+    # base condition
+    if itr == 1 or itr == 2:  
+        return True
+      # if given number divided by itr or not
+    if number % itr == 0:  
+        return False
+      # Recursive function Call
+    if Prime(number, itr - 1) == False:  
+        return False
+
+    return True
+
+num = 13
+
+itr = int(sqrt(num) + 1)
+
+print(Prime(num, itr))
+
+
+
